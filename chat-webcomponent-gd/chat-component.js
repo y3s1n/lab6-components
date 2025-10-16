8ff56235a812cc5f68c6a704acd2df3b35e09c98
Yesin Qasem <yesinq88@gmail.com>
2025-10-15 19:38:30 -0700

    Added JSDocs

--- chat-webcomponent-gd/chat-component.js
+++ chat-webcomponent-gd/chat-component.js
@@ -222,6 +222,9 @@ class chatInterface extends HTMLElement {
         this.setupEventListeners();
     }
 
+    /**
+     * Connects listeners to track button clicks and the enter key
+     */
     setupEventListeners() {
          this.button.addEventListener('click', ()  => { this.send();});
          this.textarea.addEventListener('keydown', (e) => {
@@ -234,6 +237,11 @@ class chatInterface extends HTMLElement {
     }
 
     
+    /**
+     * Activates when a user triggers the event listener in init, captures the value in the
+     * textarea, clears the textarea, then adds the user message to the chat window by calling the addToChat window function. 
+     * Then it calls on the processMessage function to get a response from eliza
+     */
     send() {
    
         const text = this.textarea.value;
@@ -246,6 +254,12 @@ class chatInterface extends HTMLElement {
 
     }
 
+    /**
+     * Captures message and speaker, then builds the dom node
+     *  and inserts it directly to the tree
+     * @param {String} text the value from the message box
+     * @param {String} speaker either bot or user
+     */
     addToChatWindow(text, speaker) {
     
         const theSpeaker = document.createElement('div');
@@ -262,15 +276,29 @@ class chatInterface extends HTMLElement {
 
     }
 
+    /**
+     *  Sends bot response to addToChatWindow with the bot as the speaker
+     * @param {String} response bot response from eliza
+     */
     showResponse(response) {
         
         this.addToChatWindow(response, 'bot');
     }
     
+    /**
+     * gets user message from processMessage function and sends it to pattern recognition function in eliza
+     * "getBotResponse"
+     * @param {String} text user message from textarea
+     * @returns {string} bot response from eliza
+     */
     getResponse(text) {
         return getBotResponse(text);
     }
     
+    /**
+     * Takes user message and sends it to the getResponsefunction 
+     * @param {string} text user message from textarea
+     */
     processMessage(text) {
         let response = this.getResponse(text);
     
